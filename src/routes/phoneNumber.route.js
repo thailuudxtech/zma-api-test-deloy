5df6689a0fac64f968b93331d6fc2b216cb165e6
ThaiLuu92 <140800675+ThaiLuu92@users.noreply.github.com>
2024-01-25 11:36:10 +0700

    a

--- src/routes/phoneNumber.route.js
+++ src/routes/phoneNumber.route.js
@@ -8,12 +8,13 @@ const phoneNumberRouter = express.Router();
 phoneNumberRouter.post('/', async (req, res) => {
   try {
   
-    const userAccessToken = req.body;
+    const userAccessToken = "yKH03xpWOIlmGKaDj-5WAln6JmYt-nzYlWnbDuBUFrN99rOAczC4Kzq2Mo35mmDW-IPL1hBp27BzLtCEklGuHRS2TWxbvJmew0vaVUEjC2dRL1zymh1DA-La95lLetmwp60FIVMRBm7DEMLPtjWo7la9U6Zig38rgmjq5v-gFpA13byGkCq0IArgBns4a7vbaJCr1O_7VM-5CnKgWvSrSufgLWMHdWTDsZrBFSxO6N3o57SDtR5RVDiF6rhcrZG5mJyLPlVXFH_B4sTNvzWe2xKjIogGXoTplMTfDeQE4tsNNsSOc9izFgrf4bxXjsmVzr4_PBYsO2IIOHO3Wxve8xzt9WdsWdfJvICo8_lJJKBnCMTclfPEBM5_anaIklbeB0";
     console.log(1111111,userAccessToken);
     console.log(9999999);
     const endpoint = 'https://graph.zalo.me/v2.0/me/info';
-    const token = 100;
+    const token = '100';
     const secretKey = 'Q6EX64TCVpLF687uEVKV';
+    
     const options = {
       url: endpoint,
       headers: {
