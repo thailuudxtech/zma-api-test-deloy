be3a82e83b3924f94e17df5d4773bbb43be341ed
ThaiLuu92 <140800675+ThaiLuu92@users.noreply.github.com>
2024-01-25 10:30:48 +0700

    a

--- src/routes/phoneNumber.route.js
+++ src/routes/phoneNumber.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const axios = require('axios');
+const request = require("request");
+
 
 const phoneNumberRouter = express.Router();
 
@@ -7,27 +9,41 @@ phoneNumberRouter.post('/', async (req, res) => {
   try {
   
     const userAccessToken = req.body;
-    console.log(777777,req.body);
-
     const endpoint = 'https://graph.zalo.me/v2.0/me/info';
     const token = '2929941295009952945';
     const secretKey = 'jylgDFQihnwppVSszgwb6QZB-JBkhwGUhyltFJOnS5Hh6_Qlj1i';
 
-    const params = {
-      access_token: userAccessToken,
-      code: token,
-      secret_key: secretKey,
+    // const params = {
+    //   access_token: userAccessToken,
+    //   code: token,
+    //   secret_key: secretKey,
+    // };
+    const options = {
+      url: endpoint,
+      headers: {
+        access_token: userAccessToken,
+        code: token,
+        secret_key: secretKey,
+      },
     };
+    
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error("Error:", error);
+      } else {
+        console.log("Response Code:", response.statusCode);
+        console.log("Response Body:", body);
+      }
+    });
+    // const response = await axios.get(endpoint, { params: params });
 
-    const response = await axios.get(endpoint, { params: params });
+    // console.log('Response Code:', response.status);
+    // console.log('Response Body:', response);
 
-    console.log('Response Code:', response.status);
-    console.log('Response Body:', response.data);
+    // const data = response.data;
+    // const phoneNumber = data.phone || 'Phone number not available';
 
-    const data = response.data;
-    const phoneNumber = data.phone || 'Phone number not available';
-
-    res.json({ phoneNumber });
+    // res.json({ phoneNumber });
   } catch (error) {
     console.error('Error getting phone number:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
